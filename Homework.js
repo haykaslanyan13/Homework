cc39530e5f7b607e115af79c30b4c8bbb98e7876
haykaslanyann <85225809+haykaslanyann@users.noreply.github.com>
2021-07-09 19:23:14 +0400

    Add files via upload

--- Homework.js
+++ Homework.js
@@ -1,58 +1,120 @@
 1.
-function creatArray (a,b,step){
-    let array = [];
-    for(a; a <= b; a += step){
-        array.push(a);
-    }
-    return array;
+function seperateOddAndEven (array){
+    let oddArray = [];
+    let evenArray = [];
+        for(let i = 0; i < array.length; i++){
+  	        if(array[i] % 2 === 0){
+                evenArray.push(array[i]);
+            }else{
+                oddArray.push(array[i]); 
+            }
+        }return [oddArray,evenArray]
 }
 
 2.
-function maxElemInArr(array){
-    let max = array[0];
-    for(let i = 0; i < array.length; i++){
-        if (array[i] < array[i+1] && array[i+1] > max){
-            max = array[i+1];
+function selCorrSublist(word,arr){
+    let array = [];
+    for(let i = 0; i < arr.length; i++){
+        let z = "";
+        let f = word;
+        for(let j = 0; j < arr[i].length; j++){
+            if(arr[i].length !== word.length){
+                break;
+            }
+            for(let k = 0; k < f.length; k++){
+                if(arr[i][j] === f[k]){
+                    z += arr[i][j];
+                    f = f.substring(0,k) + f.substring(k+1,f.length)
+
+                }else{
+                    z += "";
+                }
+            }
         }
-    }return max;
+        if(z.length === word.length){
+            array.push(arr[i]);
+        }
+    }return array; 
 }
 
-3.
-function revArray(array){
-    let length = array.length;
-    for(let i = length; i > 0; i--){
-        array.push(array[i-1]);
-    }
-    for(let j = 0; j < length; j++){
-        array.shift(array[0]);
-    }return array;
-}
+ Second.versia
 
-4.
-function newArray(array){
-    let arr = [];
-    for(let i = 0; i < array.length - 1; i++){
-        arr.push(array[i] * array[i+1]);
-    }return arr;
-}
-
-5.
-function newArr(array,number){
-    let length = array.length;
-    for(let i = 0; i < length; i++){
-        if(array[i] !== number ){
-            array.push(array[i]);
-        }else{
-            array.push(); 
-         }
-     }
-     for(let j = 0; j < length; j++){
-        array.shift(0);
-     }
-     if(array.length === length){
-        array.push(number);
-        return array;
-     }else{
-         return array;
+function selCorrSublist1(word,arr){
+  let array = [];
+  for(let i = 0; i < arr.length; i++){
+    let z = "";
+    let f = word;
+    for(let j = 0; j < arr[i].length; j++){
+      if(arr[i].length !== word.length){
+        break;
       }
+      for(let k = 0; k < f.length; k++){
+        if(arr[i][j] === f[k]){
+          z += arr[i][j];
+          let n = "";
+          for(let l = 0; l < f.length; l++){
+            if(l !== k){
+              n += f[l];
+            }
+          }f = n;
+        }else{
+          z += "";
+        }
+      }
+    }
+    if(z.length === word.length){
+      array.push(arr[i]);
+    }
+  }return array ;
 }
+
+    3.
+    function removeApearance(firstString,secondString){
+        let result = "";
+        for(let i = 0; i < firstString.length; i++){
+          for(let j = 0; j < secondString.length + 1; j++){
+              if( undefined === secondString[j] && j === secondString.length){
+                   result +=  "";
+                 i--
+                 break;
+              }else if(firstString[i] === secondString[j]){
+                        i++;
+                        continue;
+              }else{
+                result += firstString[i];
+              }
+              break;
+          }
+        }return(result);
+        }
+
+        4.
+        function onlyUniqElem (arr){
+          let newArr = [];
+          for( let i = 0; i < arr.length; i++){
+            for(let j = 0; j <= newArr.length; j++){
+              if(arr[i] !== newArr[j] && j === newArr.length){
+                newArr.push(arr[i]);
+                break;
+              }
+              if(arr[i] !== newArr[j]){
+                continue;
+              }
+              else{
+                break;
+              }
+            }
+          }return newArr;
+        }
+
+        5.
+        function changeSymbols (string,firstSymbol,secondSymbol){
+          let newString = "";
+          for(let i = 0; i < string.length; i++){
+              if(string[i] === firstSymbol){
+                  newString += secondSymbol;
+              }else{
+                  newString += string[i];
+              }
+          }return newString;
+      }
